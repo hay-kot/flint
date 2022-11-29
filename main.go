1e3b4da8aa63f11119ebaef24d25432cc7b6fcf7
Hayden <64056131+hay-kot@users.noreply.github.com>
2022-11-29 10:12:40 -0900

    embed example + CLI updates

--- main.go
+++ main.go
@@ -9,14 +9,42 @@ import (
 	"sort"
 	"time"
 
+	_ "embed"
+
 	"github.com/hay-kot/flint/flint"
 	"github.com/urfave/cli/v2"
 )
 
+//go:embed example.yml
+var example []byte
+
+var (
+	version = "0.1.0"
+	build   = "dev"
+	date    = "unknown"
+)
+
+func pathResolver(cwd string, path string) string {
+	if filepath.IsAbs(path) {
+		return path
+	}
+
+	return filepath.Join(cwd, path)
+}
+
 func main() {
 	app := &cli.App{
-		Name:  "flint",
-		Usage: "extensible frontmatter linter",
+		Name:    "flint",
+		Version: fmt.Sprintf("%s (%s), built at %s", version, build, date),
+		Usage:   "extensible frontmatter linter",
+		Flags: []cli.Flag{
+			&cli.PathFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "path to config file",
+				Value:   "flint.yml",
+			},
+		},
 		Action: func(c *cli.Context) error {
 			var err error
 			start := time.Now()
@@ -34,9 +62,9 @@ func main() {
 				return fmt.Errorf("failed to get current working directory: %w", err)
 			}
 
-			confpath := filepath.Join(cwd, "flint.yml")
+			confpath := pathResolver(cwd, c.String("config"))
 			if _, err := os.Stat(confpath); os.IsNotExist(err) {
-				return fmt.Errorf("failed to find flint.yml in current working directory: %w", err)
+				return fmt.Errorf("failed to find flint.yml at %q: %w", confpath, err)
 			}
 
 			conffile, err := os.OpenFile(confpath, os.O_RDONLY, 0)
@@ -76,6 +104,26 @@ func main() {
 			fmt.Println(flint.StyleLightGray.Render((fmt.Sprintf("\n✨ flint took %s\n", time.Since(start)))))
 
 			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "init",
+				Usage: "create a flint.yml file in the current working directory",
+				Action: func(c *cli.Context) error {
+					target := c.String("config")
+
+					if _, err := os.Stat(target); !os.IsNotExist(err) {
+						return fmt.Errorf("flint.yml already exists in current working directory")
+					}
+
+					err := os.WriteFile(target, example, 0x644)
+					if err != nil {
+						return fmt.Errorf("failed to create flint.yml: %w", err)
+					}
+
+					return nil
+				},
+			},
 		},
 	}
 
